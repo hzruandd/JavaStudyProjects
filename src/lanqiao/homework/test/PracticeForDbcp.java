b49289baa4ceca80597ccfb5d7713ade6662885a
Doctor邓 <Doctor@10.216.130.122>
2016-06-01 17:41:47 +0800

    c3p0 jar包的导入

--- src/lanqiao/homework/test/PracticeForDbcp.java
+++ src/lanqiao/homework/test/PracticeForDbcp.java
@@ -4,9 +4,11 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 
 import lanqiao.homework.util.SqlUtil;
 import lanqiao.homework.util.impl.DBCPUtil;
+import lanqiao.homework.util.impl.DBUtil;
 
 /**
  * 练习数据库连接池Dbcp的使用
@@ -16,8 +18,40 @@ import lanqiao.homework.util.impl.DBCPUtil;
 public class PracticeForDbcp {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
+
+		//1.通过不同方式操作数据库
+		Date a = new Date();
+		test();
+		Date b = new Date();
+		System.out.println(b.getTime() - a.getTime());
+		
+		//2.通过DBCP连接池方式操作数据库
+		Date c = new Date();
+		testByDBCP();
+		Date d = new Date();
+		System.out.println(d.getTime() - c.getTime());
+	}
+	
+	public static void testByDBCP() {
 		SqlUtil sqlUtil = new DBCPUtil();
+		Connection conn = sqlUtil.getConn();
+		try {
+			CallableStatement cs = conn.prepareCall("BEGIN testStu(?); END;");
+			cs.registerOutParameter(1, -10);
+			cs.execute();
+			ResultSet rs = (ResultSet) cs.getObject(1);
+			while (rs.next()) {
+				System.out.println(rs.getString("stu_name"));
+			}
+			rs.close();
+		} catch (SQLException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+	}
+	
+	public static void test() {
+		SqlUtil sqlUtil = new DBUtil();
 		Connection conn = sqlUtil.getConn();
 		try {
 			CallableStatement cs = conn.prepareCall("BEGIN testStu(?); END;");
